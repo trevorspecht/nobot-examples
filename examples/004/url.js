1f2d48e9e7371e1c6d580f517f523aae1beb7fca
Trevor Specht <trevor.specht@mapbox.com>
2020-03-29 23:50:41 -0400

    004

--- examples/004/url.js
+++ examples/004/url.js
@@ -1,1 +1,20 @@
 const url = require('url');
+
+const args = process.argv.slice(2);
+
+const [urlEntered] = args;
+
+if (urlEntered === undefined) {
+    console.error('Please enter a URL');
+    process.exit(0);
+}
+
+const {
+    protocol, slashes, host, query, href
+} = url.parse(urlEntered);
+
+console.log(`protocol: ${protocol}`);
+console.log(`slashes: ${slashes}`);
+console.log(`host: ${host}`);
+console.log(`query: ${query}`);
+console.log(`HREF: ${href}`);
